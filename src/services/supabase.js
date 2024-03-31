70ba15c7f38fb78749df06baf2422c7e9ec13cd5
Sohaib bn <87204575+Sohaib-Bn@users.noreply.github.com>
2024-03-31 23:03:41 +0200

    fix supabase key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -4,7 +4,7 @@ const supabaseAuthUrl = "https://ptgevxelcojvdjvctirn.supabase.co";
 
 const supabaseServiveRoleKey = import.meta.env
   .VITE_REACT_APP_API_SERVICE_ROLE_KEY;
-const supabaseAuthKey = import.meta.env.VITE_REACT_APP_SUPABASE_AUTH_KEY;
+const supabaseAuthKey = import.meta.env.VITE_REACT_APP_SUPABASE_AUTH_API_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseServiveRoleKey, {
   auth: {
